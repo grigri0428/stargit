3f7d381c38d84b466d4707b9e6377c55c959fbee
grigri0428 <fuji_e_saku@hotmail.com>
2012-12-17 12:28:38 +0900

    上下キーへの操作追加

--- gittest/Assets/move.js
+++ gittest/Assets/move.js
@@ -8,5 +8,6 @@ function Update () {
 
 
 transform.rotation *=Quaternion.AngleAxis(Input.GetAxis("Horizontal")*30*Time.deltaTime,Vector3(0,0,1));
+transform.rotation *=Quaternion.AngleAxis(Input.GetAxis("Vertical")*30*Time.deltaTime,Vector3(1,0,0));
 
 }
